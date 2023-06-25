58f334dc015e7acf800d7d07e4578dd51f0eb66a
Daniil <post@windhelm.ru>
2023-06-25 16:27:52 +0300

    Добавлены тесты

--- Papyc/task0/test/tests.cc
+++ Papyc/task0/test/tests.cc
@@ -2,7 +2,7 @@
 
 #include <algorithm>
 
-#include "../list.h"
+#include "../src/list.h"
 
 TEST(tests_list, correct_argument) {
   List test("abcde");
@@ -30,6 +30,19 @@ TEST(tests_list, correct_argument_long) {
   EXPECT_EQ(res, data);
 }
 
+TEST(tests_list, correct_argument_escape) {
+  std::string data = "\r\n\t\v\"\'\\\?\a";
+  List test(data);
+  List reverse_test = test.Reverse();
+  std::string res;
+  for (Node* print = reverse_test.GetNullNode().next_node; print != nullptr;) {
+    res.push_back(print->symbol);
+    print = print->next_node;
+  }
+  std::reverse(data.begin(), data.end());
+  EXPECT_EQ(res, data);
+}
+
 TEST(tests_list, empty) {
   List test("");
   List reverse_test = test.Reverse();
@@ -39,4 +52,14 @@ TEST(tests_list, empty) {
     print = print->next_node;
   }
   EXPECT_EQ(res, "");
+}
+
+TEST(tests_list, size) {
+  List test_1("abcde");
+  std::string data = "\r\n\t\v\"\'\\\?\a";
+  List test_2(data);
+  List test_3("");
+  EXPECT_EQ(test_1.Size(), 5);
+  EXPECT_EQ(test_2.Size(), 9);
+  EXPECT_EQ(test_3.Size(), 0);
 }
