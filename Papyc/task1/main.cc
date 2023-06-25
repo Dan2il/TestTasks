fc52356c3e489d847cff5eeff7692c566ba9545c
Daniil <post@windhelm.ru>
2023-06-25 16:35:57 +0300

    Добавлен Make и тесты

--- Papyc/task1/main.cc
+++ Papyc/task1/main.cc
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <iostream>
 #include <string>
 
 bool CheckCorrectPositionBrackets(const std::string& expression) {
@@ -18,8 +19,21 @@ bool CheckCorrectPositionBrackets(const std::string& expression) {
 
 int main() {
   assert(CheckCorrectPositionBrackets("2+2") == true);
+  assert(CheckCorrectPositionBrackets("(1+2)*4") == true);
+  assert(CheckCorrectPositionBrackets("(1+2)*4+3") == true);
+  assert(CheckCorrectPositionBrackets("((1+2)*4)+3") == true);
   assert(CheckCorrectPositionBrackets(
-             "assert(CheckCorrectPositionBrackets(\"2 + 2\") == true)") ==
-         true);
+             "15/(7-(1+1))*3-(2+(1+1))*15/(7-(200+1))*3-(2+(1+1))*(15/"
+             "(7-(1+1))*3-(2+(1+1))+15/(7-(1+1))*3-(2+(1+1)))") == true);
+  assert(CheckCorrectPositionBrackets("") == true);
+  assert(CheckCorrectPositionBrackets("-456-(-2*-56)+(+(-23)/45)") == true);
+  assert(CheckCorrectPositionBrackets("sin(cos(111))+tan(sqrt(222))-acos(asin("
+                                      "333))*atan(444)/ln(log(555))") == true);
+  assert(CheckCorrectPositionBrackets("-(-(-4,4-1,1))") == true);
+
   assert(CheckCorrectPositionBrackets("(234cds+23r))") == false);
+  assert(CheckCorrectPositionBrackets("3+4*2/(1-5)^2)") == false);
+  assert(CheckCorrectPositionBrackets("(") == false);
+
+  std::cout << "success" << std::endl;
 }
