96fd477c0e62b4b31f39b53952b7e9cd45973c58
Daniil <post@windhelm.ru>
2023-07-03 10:15:26 +0300

    Добавлены класс button

--- TopSystems/main.cpp
+++ TopSystems/main.cpp
@@ -1,76 +1,83 @@
 #include <SFML/Graphics.hpp>
 
+#include "source/button.h"
 #include "source/painter.h"
 
+void MenuProcessing(sf::RenderWindow& window, sf::Event& event);
+
 int main() {
-  sf::RenderWindow window(sf::VideoMode(800, 600), "Shapes",
-                          sf::Style::Default);
+  sf::RenderWindow window(sf::VideoMode(800, 600), "Shapes", sf::Style::Close);
 
-  // Rectangle shape(50, 10);
-  // shape.SetContour(20, sf::Color::White);
-  // shape.SetPosition(35, 50);
-  // shape.SetTexture("Texture/1.png");
-  // shape.SetPosition(100, 100);
-
-  // Polygon shape(200, 10, sf::Color::Blue);
-  // shape.SetContour(20, sf::Color::Black);
-  // shape.SetPosition(50, 50);
-  // shape.SetTexture("Texture/1.png");
-
-  sf::Texture for_button_circle;
-  for_button_circle.loadFromFile("ButtonTexture/button_sozdat-krug.png");
-  sf::Sprite button_circle(for_button_circle);
-  button_circle.setPosition(600, 100);
-
-  sf::Texture for_button_rectangle;
-  for_button_rectangle.loadFromFile("ButtonTexture/button_kvadrat.png");
-  sf::Sprite button_rectangle(for_button_rectangle);
-  button_rectangle.setPosition(600, 150);
-
-  sf::Texture for_button_clear;
-  for_button_clear.loadFromFile("ButtonTexture/button_ochistit-pole.png");
-  sf::Sprite button_clear(for_button_clear);
-  button_clear.setPosition(600, 500);
+  Button button_circle("ButtonTexture/button_sozdat-krug.png", 600, 100);
+  Button button_square("ButtonTexture/button_kvadrat.png", 600, 150);
+  Button button_rectangle("ButtonTexture/button_pryamougolnik.png", 600, 200);
+  Button button_triangle("ButtonTexture/button_treugolnik.png", 600, 250);
+  Button button_polygon("ButtonTexture/button_mnogougolnik.png", 600, 300);
+  Button button_clear("ButtonTexture/button_ochistit-pole.png", 600, 500);
 
   bool update_display = true;
 
   while (window.isOpen()) {
     window.clear(sf::Color::White);
-    button_circle.setColor(sf::Color::White);
 
-    window.draw(button_circle);
-    window.draw(button_rectangle);
-    window.draw(button_clear);
+    window.draw(button_circle.Get());
+    window.draw(button_square.Get());
+    window.draw(button_rectangle.Get());
+    window.draw(button_triangle.Get());
+    window.draw(button_polygon.Get());
+    window.draw(button_clear.Get());
 
     sf::Event event;
     while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed) {
-        window.close();
-      } else if (event.type == sf::Event::MouseButtonPressed) {
-        if (event.mouseButton.button == sf::Mouse::Left) {
-          if (event.mouseButton.x > 600 && event.mouseButton.x < 800 &&
-              event.mouseButton.y > 100 && event.mouseButton.y < 150) {
-            Circle shape(200, sf::Color::Black);
-            shape.SetPosition(10, 10);
-            window.draw(shape.Get());
-            window.display();
-          } else if (event.mouseButton.x > 600 && event.mouseButton.x < 800 &&
-                     event.mouseButton.y > 150 && event.mouseButton.y < 200) {
-            Rectangle shape(200, 200, sf::Color::Black);
-            shape.SetPosition(10, 10);
-            window.draw(shape.Get());
-            window.display();
-          } else if (event.mouseButton.x > 600 && event.mouseButton.x < 800 &&
-                     event.mouseButton.y > 500 && event.mouseButton.y < 550) {
-            window.display();
-          }
-        }
-      }
+      MenuProcessing(window, event);
     }
 
     if (update_display == true) {
       window.display();
       update_display = false;
+    }
+  }
+}
+
+void MenuProcessing(sf::RenderWindow& window, sf::Event& event) {
+  if (event.type == sf::Event::Closed) {
+    window.close();
+  } else if (event.type == sf::Event::MouseButtonPressed) {
+    if (event.mouseButton.button == sf::Mouse::Left) {
+      if (event.mouseButton.x > 600 && event.mouseButton.x < 800 &&
+          event.mouseButton.y > 100 && event.mouseButton.y < 150) {
+        Circle shape(200, sf::Color::Black);
+        shape.SetPosition(10, 10);
+        window.draw(shape.Get());
+        window.display();
+      } else if (event.mouseButton.x > 600 && event.mouseButton.x < 800 &&
+                 event.mouseButton.y > 150 && event.mouseButton.y < 200) {
+        Rectangle shape(200, 200, sf::Color::Black);
+        shape.SetPosition(10, 10);
+        window.draw(shape.Get());
+        window.display();
+      } else if (event.mouseButton.x > 600 && event.mouseButton.x < 800 &&
+                 event.mouseButton.y > 200 && event.mouseButton.y < 250) {
+        Rectangle shape(200, 400, sf::Color::Black);
+        shape.SetPosition(10, 10);
+        window.draw(shape.Get());
+        window.display();
+      } else if (event.mouseButton.x > 600 && event.mouseButton.x < 800 &&
+                 event.mouseButton.y > 250 && event.mouseButton.y < 300) {
+        Polygon shape(200, 3, sf::Color::Black);
+        shape.SetPosition(10, 10);
+        window.draw(shape.Get());
+        window.display();
+      } else if (event.mouseButton.x > 600 && event.mouseButton.x < 800 &&
+                 event.mouseButton.y > 300 && event.mouseButton.y < 350) {
+        Polygon shape(200, 7, sf::Color::Black);
+        shape.SetPosition(10, 10);
+        window.draw(shape.Get());
+        window.display();
+      } else if (event.mouseButton.x > 600 && event.mouseButton.x < 800 &&
+                 event.mouseButton.y > 500 && event.mouseButton.y < 550) {
+        window.display();
+      }
     }
   }
 }
